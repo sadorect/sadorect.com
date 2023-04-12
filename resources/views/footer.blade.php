6470f8fefe56b2e0613407ccdd23ceb688857ea0
OluwaTosin Omojola <sadorect@gmail.com>
2023-04-12 02:44:38 +0100

    google ads in a blade file

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,15 +1,4 @@
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4048280593346646"
-     crossorigin="anonymous"></script>
-<!-- Sadorect Default -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-4048280593346646"
-     data-ad-slot="6944547606"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+@include('google-ads.php');
     
     
     <!-- Footer Start -->
