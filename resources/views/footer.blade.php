8737eafa32c94c3a185c22a6df303d68f9c4d6a7
OluwaTosin Omojola <sadorect@gmail.com>
2023-04-12 02:08:59 +0100

    added an adsense ad block to footer blade

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,4 +1,17 @@
-
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4048280593346646"
+     crossorigin="anonymous"></script>
+<!-- Sadorect Default -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-4048280593346646"
+     data-ad-slot="6944547606"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+    
+    
     <!-- Footer Start -->
     <div class="container-fluid bg-primary text-secondary p-5">
         <div class="row g-5">
