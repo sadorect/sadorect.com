37adf3b716cab11b13b0a46f2c2d70de7a830220
Tosin Omojola <sadorect@gmail..com>
2025-01-18 22:56:53 +0000

    footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -44,4 +44,3 @@
 <!-- Back to Top -->
 <a href="#" class="btn btn-lg btn-primary btn-lg-square rounded-circle back-to-top"><i class="bi bi-arrow-up"></i></a>
 <!-- Ad Component placed after main footer content -->
-<x-smart-ad-component slug="Google-Ads"/>
