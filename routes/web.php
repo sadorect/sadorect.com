ecb9c43327b88e6d1db521e24059928627d45826
Tosin Omojola <sadorect@gmail..com>
2025-01-19 03:07:04 +0000

    routing

--- routes/web.php
+++ routes/web.php
@@ -35,9 +35,7 @@ Route::get('/quote', [PageController::class, 'quote'])->name('quote');
 Route::get('/blog-detail', [PageController::class, 'blogDetail'])->name('blogDetail');
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -45,17 +43,14 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-//Blog Post Routes
-Route::get('/create-post', [PostController::class, 'createPost'])->name('create-post');
-Route::post('/create-post', [PostController::class, 'storeNewPost'])->name('create.post');
-Route::get('/post/{post}', [PostController::class, 'viewSinglePost'])->name('post');
+
 
 Route::get('/portfolio', function () {
     return view('portfolio');
 })->name('portfolio');
 
 Route::prefix('admin')->name('admin.')->middleware(['auth', 'admin'])->group(function () {
-    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('portfolio', PortfolioController::class);
     Route::get('admin/portfolio/{portfolio}/refresh-thumbnail', [PortfolioController::class, 'refreshThumbnail'])
     ->name('portfolio.refresh-thumbnail');
